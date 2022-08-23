989774f8878e0e97347b4f561b1cb62180e302e4
Sriharsha <sriharshasaligrama@gmail.com>
2022-08-23 21:55:29 +0530

    Updated CheckOut

--- src/Components/CheckOut.js
+++ src/Components/CheckOut.js
@@ -1,12 +1,20 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import DataTable from 'react-data-table-component';
 import { Link } from 'react-router-dom'
+import Button from 'react-bootstrap/Button';
+import { emptyCart } from '../Redux/Actions/CartActions';
 
 export const CheckOut = props => {
     const {cartItems}=useSelector(state=>state)
     const subTotalArray=cartItems.map(item=>item.price)
     const subTotal=subTotalArray.length>0?subTotalArray.reduce((a,b)=>a+b):0
+    const dispatch=useDispatch()
+
+    const handleEmptyCart= ()=>{
+        dispatch(emptyCart())
+    }
+
     const columns = [
         {
             name: 'Cart Items',
@@ -29,7 +37,7 @@ export const CheckOut = props => {
 
     return (
         <div >
-            <Link to='/'>Home</Link><br/>
+            <Link to='/'><Button variant="link" onClick={handleEmptyCart}>Home</Button></Link><br/>
             {cartItems.length>0?'Order placed successfully':'No orders placed'}<br/>
             <>
                 <DataTable columns={columns} data={data} customStyles={{rows: {style: {height: '70px'}}}} disabled/>
